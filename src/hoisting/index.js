ad7142688eff4428bccab9f5c2a837445183c7ad
davejuanes <dmjuanes_@hotmail.es>
2022-08-17 16:39:34 -0400

    MODIFICACION DE HOISTING Y DEBBUGER

---
+++ src/hoisting/index.js
@@ -0,0 +1,16 @@
+
+/* var nameOfDog; //undefined
+console.log(nameOfDog)
+
+var nameOfDog = 'Niels'
+console.log(nameOfDog); */
+
+
+// var niels // undefined
+nameOfDog()
+
+function nameOfDog() {
+    console.log(`El mejor perrito es ${Niels}`);
+}
+
+var Niels = 'Nielsburg'
